d8f854de0e85b00c81d31a1cb734ef8cf1b94a9a
Gyu-Yeon <idsopy@nate.com>
2023-01-10 13:40:59 +0900

    17

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import Dropdown from "./components/Dropdown";
 import { useState } from "react";
 
 function App() {
-  const options = [
-    { label: "Red", value: "Red" },
-    { label: "Blue", value: "Blue" },
-    { label: "Green", value: "Green" },
-  ];
-  const [selectedOption, setSelectedOption] = useState("Select...");
-
-  const handleSelectClick = (value) => {
-    setSelectedOption(value);
-  };
-
   return (
     <div className="flex">
-      <Dropdown
-        options={options}
-        selectedOption={selectedOption}
-        handleSelectClick={handleSelectClick}
-      />
-      <Dropdown
-        options={options}
-        selectedOption={selectedOption}
-        handleSelectClick={handleSelectClick}
-      />
+      <h1>Navigation Bar</h1>
     </div>
   );
 }
---
+++ src/pages/Dropdownpage.js
@@ -0,0 +1,32 @@
+import Dropdown from "../components/Dropdown";
+import { useState } from "react";
+
+function Dropdownpage() {
+  const options = [
+    { label: "Red", value: "Red" },
+    { label: "Blue", value: "Blue" },
+    { label: "Green", value: "Green" },
+  ];
+  const [selectedOption, setSelectedOption] = useState("Select...");
+
+  const handleSelectClick = (value) => {
+    setSelectedOption(value);
+  };
+
+  return (
+    <div className="flex">
+      <Dropdown
+        options={options}
+        selectedOption={selectedOption}
+        handleSelectClick={handleSelectClick}
+      />
+      <Dropdown
+        options={options}
+        selectedOption={selectedOption}
+        handleSelectClick={handleSelectClick}
+      />
+    </div>
+  );
+}
+
+export default Dropdownpage;
