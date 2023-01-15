3dd4d71ef72b9e22d6ceb45fb88904b158c22b8d
Gyu-Yeon <idsopy@nate.com>
2023-01-16 03:22:09 +0900

    21

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dropdownpage from "./pages/Dropdownpage";
 import AccordionPage from "./pages/AccordionPage";
 import ButtonPage from "./pages/ButtonPage";
 import Modalpage from "./pages/Modalpage";
+import TablePage from "./pages/TablePage";
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
         </Route>
         <Route path="/modal">
           <Modalpage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
         </Route>
       </div>
     </div>
